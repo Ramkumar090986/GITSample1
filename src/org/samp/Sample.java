0a76d8f8d2efd037a7c030f59a059be26dfad97d
Ramkumar090986 <ramkumar8687@gmail.com>
2022-11-28 11:41:08 +0530

    New code added

---
+++ src/org/samp/Sample.java
@@ -0,0 +1,20 @@
+package org.samp;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to GIT");
+		
+		int a=10,b=5,sum,avg;
+		
+		sum=a+b;
+		
+		System.out.println("Sume is:"+sum);
+		
+		avg=sum/2;
+		
+		System.out.println("Average:"+avg);
+	}
+
+}
